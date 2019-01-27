2050a89fd5ef6403aca2729b6ed6786a9198e36f
tathanhxuan <taxuanus@gmail.com>
2019-01-27 16:02:47 -0600

    Add User properties

--- src/main/java/com/hr/domain/User.java
+++ src/main/java/com/hr/domain/User.java
@@ -1,5 +1,68 @@
 package com.hr.domain;
 
-public class User {
- // test
+public abstract class User {
+	int id;
+	int group_id;
+	String email;
+	String address;
+	String nick_name;
+	String name;
+	String mobile;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getGroup_id() {
+		return group_id;
+	}
+
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNick_name() {
+		return nick_name;
+	}
+
+	public void setNick_name(String nick_name) {
+		this.nick_name = nick_name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
 }
