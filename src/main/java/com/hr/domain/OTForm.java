8ed50e9342e64a35d34df9c364bff425ff0c6f4b
Huu Thai Ho <mekhuc82@gmail.com>
2019-01-29 01:29:59 +0700

    oo

--- src/main/java/com/hr/domain/OTForm.java
+++ src/main/java/com/hr/domain/OTForm.java
@@ -2,28 +2,35 @@ package com.hr.domain;
 
 import java.util.Date;
 
+import com.hr.domain.approval.IApproverList;
+
 public class OTForm extends Form {
 
-	@Override
-	Boolean Submit() {
-		// TODO Auto-generated method stub
-		return null;
+	public OTForm(String formCode, String empID, FormStatus status) {
+		super(formCode, empID, status);
+		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	Boolean Approve() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	Boolean Update() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	@Override
 	public Date getDateFrom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Boolean ReSubmit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Boolean Approve(FormApprover approvalModel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Boolean Submit(IApproverList approverList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
---
+++ src/main/java/com/hr/domain/approval/IApproverList.java
@@ -0,0 +1,8 @@
+package com.hr.domain.approval;
+import java.util.ArrayList;
+import com.hr.domain.FormApprover;
+
+public interface IApproverList {
+	public ArrayList<FormApprover> getListApprover();
+}
+
