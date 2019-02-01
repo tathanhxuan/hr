73b7cc922a8635a35e30d7e3fbb23a8c5cd2a5e8
tathanhxuan <taxuanus@gmail.com>
2019-02-01 11:29:11 -0600

    S user

--- src/main/java/com/hr/domain/SystemUser.java
+++ src/main/java/com/hr/domain/SystemUser.java
@@ -1,9 +1,28 @@
 package com.hr.domain;
 
-public abstract class SystemUser {
+import java.io.Serializable;
+
+public class SystemUser implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String id;
 	String user_name;
 	String password;
-	String security_level;
+	String empID;
+	Boolean isApprover;
+	Boolean isHR;
+
+	public SystemUser(String id, String user_name, String password, String empID, Boolean isApprover, Boolean isHR) {
+		super();
+		this.id = id;
+		this.user_name = user_name;
+		this.password = password;
+		this.empID = empID;
+		this.isApprover = isApprover;
+		this.isHR = isHR;
+	}
 
 	public String getUser_name() {
 		return user_name;
@@ -21,12 +40,41 @@ public abstract class SystemUser {
 		this.password = password;
 	}
 
-	public String getSecurity_level() {
-		return security_level;
+	public String getEmpID() {
+		return empID;
 	}
 
-	public void setSecurity_level(String security_level) {
-		this.security_level = security_level;
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+
+	public Boolean getIsApprover() {
+		return isApprover;
+	}
+
+	public void setIsApprover(Boolean isApprover) {
+		this.isApprover = isApprover;
+	}
+
+	public Boolean getIsHR() {
+		return isHR;
+	}
+
+	public void setIsHR(Boolean isHR) {
+		this.isHR = isHR;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "[id: " + id + ", User Name: " + user_name.toString() + ", Is Approver: " + isApprover + ", Is HR: " + isHR + ", empID: " + empID.toString() + "]";
 	}
 
 }
