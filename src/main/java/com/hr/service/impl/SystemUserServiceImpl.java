c96d0bee12667c43d2916b987f05a877bdd40b2e
tathanhxuan <taxuanus@gmail.com>
2019-02-01 11:32:17 -0600

    Update

--- src/main/java/com/hr/service/impl/SystemUserServiceImpl.java
+++ src/main/java/com/hr/service/impl/SystemUserServiceImpl.java
@@ -4,18 +4,22 @@ import java.io.Serializable;
 
 import com.hr.domain.SystemUser;
 
-public class SystemUserServiceImpl extends SystemUser implements Serializable {
+public class SystemUserServiceImpl implements Serializable {
 	String id;
 	String user_name;
 	String password;
-	String security_level;
+	String empID;
+	Boolean isApprover;
+	Boolean isHR;
 	
-	public SystemUserServiceImpl(String id, String user_name, String password, String security_level) {
+	public SystemUserServiceImpl(String id, String user_name, String password, String empID, Boolean isApprover, Boolean isHR) {
 		super();
 		this.id = id;
 		this.user_name = user_name;
 		this.password = password;
-		this.security_level = security_level;
+		this.empID = empID;
+		this.isApprover = isApprover;
+		this.isHR = isHR;
 	}
 
 	public String getId() {
@@ -42,16 +46,32 @@ public class SystemUserServiceImpl extends SystemUser implements Serializable {
 		this.password = password;
 	}
 
-	public String getSecurity_level() {
-		return security_level;
+	public String getEmpID() {
+		return empID;
 	}
 
-	public void setSecurity_level(String security_level) {
-		this.security_level = security_level;
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+
+	public Boolean getIsApprover() {
+		return isApprover;
+	}
+
+	public void setIsApprover(Boolean isApprover) {
+		this.isApprover = isApprover;
+	}
+
+	public Boolean getIsHR() {
+		return isHR;
+	}
+
+	public void setIsHR(Boolean isHR) {
+		this.isHR = isHR;
 	}
 
 	@Override
 	public String toString() {
-		return "[id: " + id + ", User Name: " + user_name.toString() + ", security_level: " + security_level.toString() + "]";
+		return "[id: " + id + ", User Name: " + user_name.toString() + ", empID: " + empID.toString() + "]";
 	}
 }
