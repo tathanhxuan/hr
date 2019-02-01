46c92b5507db2b71c5b7b1fc42e85e0c64779e0e
Huu Thai Ho <mekhuc82@gmail.com>
2019-02-02 05:28:38 +0700

    test

--- src/main/java/com/hr/App.java
+++ src/main/java/com/hr/App.java
@@ -32,22 +32,20 @@ public class App
 {
 	
 	public static Employee loginUser;
+	public static String userRole;
 	
     @SuppressWarnings("null")
 	public static void main( String[] args ) throws IOException, ParseException
     {
     	
-    	//Employee emp = Login();
-    
-    	loginUser = new Employee("101",new Department("D01", ""));
-    	
-    	//Login();
-    	CreateLeaveForm(); 
+    	Login();
+    	LoadMenuByUserRole();
        	
     }
 
     public static Employee Login() {
     	Employee emp =null;
+    	
     	System.out.println("-----------SYSTEM LOGIN-------------:");
     	System.out.println("Username : ");
     	Scanner scanner = new Scanner(System. in);
@@ -55,14 +53,20 @@ public class App
     	System.out.println("Passwords : ");
     	String pass = scanner. nextLine();
     	
-    	
     	//verify user
+    	
+    	//Employee emp = Login();
+    	loginUser = new Employee("101",new Department("D01", ""));
+    	userRole = "APPROVER";
     	
     	return emp;
     }
     
-    public static void LoadStaffMenu() {
+    public static void LoadStaffMenu() throws ParseException {
     	
+    	Scanner scanner = new Scanner(System. in);
+       
+        
     	System.out.println("******************************************************");
     	System.out.println("******WELCOME TO HR WORKTIME MANAGEMENT SYSTEM********");
     	System.out.println("1.OT FORM");
@@ -73,9 +77,15 @@ public class App
     	System.out.println("6.EXIT");
     	System.out.println("*******************************************************");
     	System.out.println("*.SELECT : ");
+    	
+    	String keyID = scanner. nextLine(); 
+    	StaffMenuAction(keyID);
+    	
     }
     
-    public static void LoadApproverMenu() {
+    public static void LoadApproverMenu() throws ParseException {
+    	
+    	Scanner scanner = new Scanner(System. in);
     	
     	System.out.println("******************************************************");
     	System.out.println("******WELCOME TO HR WORKTIME MANAGEMENT SYSTEM********");
@@ -89,6 +99,9 @@ public class App
     	System.out.println("8.EXIT");
     	System.out.println("*******************************************************");
     	System.out.println("*.SELECT : ");
+    	
+    	String keyID = scanner. nextLine(); 
+    	ApproverMenuAction(keyID);
     }
    
     public static void LoadReportMenu() {
@@ -103,6 +116,7 @@ public class App
    	System.out.println("*.SELECT : ");
    }
    
+    // CREATE LEAVE FORM
     public static void CreateLeaveForm() throws ParseException {
     	
     	Scanner scanner = new Scanner(System. in);
@@ -138,22 +152,163 @@ public class App
         	if (f.formSubmit()) {
         		
         		System.out.println("FORM SUBMIT SUCCESSFULL");
-        		if( true) {
-          			LoadApproverMenu();
-            		}
-           		else LoadStaffMenu();
+        		LoadMenuByUserRole();
         	}
     	}
-    	else {
-    		System.out.println("ARE YOU SURE TO CANCEL? (Y/N):");
-    		String exit = scanner. nextLine();
-    		
-    		if((exit.equals("y") || exit.equals("Y")) && loginUser.getIsApprover()) {
-  			LoadApproverMenu();
-    		}
-   		else LoadStaffMenu();
-   	}
-
+    	
     }
     
+ // CREATE OT FORM
+ public static void CreateOTForm() throws ParseException {
+    	
+    	Scanner scanner = new Scanner(System. in);
+    	
+    	System.out.println("******OT REGISTRAR********");
+    	System.out.println("OT Date (mm/dd/yyyy):");
+    	String otdate = scanner. nextLine(); 
+    	System.out.println("Time from (hh:mm):");
+    	String timefrom = scanner. nextLine(); 
+    	System.out.println("Time to (hh:mm):");
+    	String timeto = scanner. nextLine(); 
+    	System.out.println("Reason:");
+    	String reason = scanner. nextLine();
+    	
+    	System.out.println("Press 'S=SUBMIT' | 'C=CANCEL'':");
+    	String command = scanner. nextLine();
+    	
+    	
+    	if(command.equals("S") ||command.equals("s")) {
+
+        	FormValidationStrategy vf = new LeaveFormValidation();
+        	OTForm f = (OTForm) FormFactory.creatForm(FormType.OVERTIME, loginUser);
+        	f.setValidationStrategy(vf);
+        	
+        	DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+        	
+        	Date otDate = format.parse(otdate);
+        
+        	
+        	f.setOTDate(otDate);;
+        	f.setTimeFrom(timefrom);
+        	f.setTimeTo(timeto);
+        	f.setDescription(reason);
+        	
+        	
+        	if (f.formSubmit()) {
+        		
+        		System.out.println("FORM SUBMIT SUCCESSFULL");
+        		LoadMenuByUserRole();
+        	}
+    	}
+    	
+    }
+    
+ public static void CreateATForm() throws ParseException {
+ 	
+ 	Scanner scanner = new Scanner(System. in);
+ 	
+ 	System.out.println("******AT REGISTRAR********");
+ 	System.out.println("Attendance Date (mm/dd/yyyy):");
+ 	String atdate = scanner. nextLine(); 
+ 	System.out.println("Time In (hh:mm):");
+ 	String timefrom = scanner. nextLine(); 
+ 	System.out.println("Time Out (hh:mm):");
+ 	String timeto = scanner. nextLine(); 
+ 	System.out.println("Reason:");
+ 	String reason = scanner. nextLine();
+ 	
+ 	System.out.println("Press 'S=SUBMIT' | 'C=CANCEL'':");
+ 	String command = scanner. nextLine();
+ 	
+ 	
+ 	if(command.equals("S") ||command.equals("s")) {
+
+     	FormValidationStrategy vf = new LeaveFormValidation();
+     	ATForm f = (ATForm) FormFactory.creatForm(FormType.ATTENDANT, loginUser);
+     	f.setValidationStrategy(vf);
+     	
+     	DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+     	
+     	Date otDate = format.parse(atdate);
+     
+     	
+     	f.setATDate(otDate);
+     	f.setTimeIn(timefrom);
+     	f.setTimeOut(timeto);
+     	f.setDescription(reason);
+     	
+     	
+     	if (f.formSubmit()) {
+     		
+     		System.out.println("FORM SUBMIT SUCCESSFULL");
+     		LoadMenuByUserRole();
+     	}
+ 	}
+ 	
+ }
+ 
+    public static void LoadMenuByUserRole() throws ParseException {
+    	switch (userRole) {
+		case "STAFF":
+			LoadStaffMenu();
+			break;
+
+		case "APPROVER":
+			LoadApproverMenu();
+			
+		case "HR":
+			
+		default:
+			break;
+		}
+    }
+    
+    public static void StaffMenuAction(String functionID) throws ParseException {
+    	
+    	switch (functionID) {
+		case "1":
+			CreateOTForm();
+			break;
+
+		case "2":
+			CreateATForm();
+			break;
+			
+		case "3":
+			CreateLeaveForm();
+			break;
+		default:
+			break;
+		}
+    }
+    
+    
+public static void ApproverMenuAction(String functionID) throws ParseException {
+    	
+    	switch (functionID) {
+		case "1":
+			CreateOTForm();
+			break;
+
+		case "2":
+			CreateATForm();
+			break;
+			
+		case "3":
+			CreateLeaveForm();
+			break;
+			
+		case "4":
+			
+		case "5":
+			
+		case "6":
+			
+		default:
+			break;
+		}
+    }
+
+
 }
+
