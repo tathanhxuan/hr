e249f7689f637a647c432193a2f309a30434a9d7
abammeke <44130985+abammeke@users.noreply.github.com>
2019-02-03 16:56:06 -0600

    Application

--- src/main/java/com/hr/domain/reportChart/ChartTemplate.java
+++ src/main/java/com/hr/domain/reportChart/ChartTemplate.java
@@ -1,11 +1,13 @@
 package com.hr.domain.reportChart;
 
-public abstract class ChartTemplate{
-	
+public abstract class ChartTemplate {
+
 	public abstract void getATStatistics();
+
 	public abstract void getOTStatistics();
+
 	public abstract void getLeaveStatistics();
-	
+
 	public final void getReportChart() {
 		System.out.println("Report Statistics");
 		getATStatistics();
